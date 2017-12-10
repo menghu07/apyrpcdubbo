7785576b15cea431b2946b9f0ab8ebfb10c2e192
apeny <812877318@qq.com>
2017-12-10 22:47:06 +0800

    dubbo注册中心

--- apydubbo-testone/apydubbo-testone-provider/src/main/java/com/apeny/provider/service/impl/DemoServiceImpl.java
+++ apydubbo-testone/apydubbo-testone-provider/src/main/java/com/apeny/provider/service/impl/DemoServiceImpl.java
@@ -2,12 +2,21 @@ package com.apeny.provider.service.impl;
 
 import com.apeny.api.service.DemoService;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by apeny on 2017/11/19.
  */
 public class DemoServiceImpl implements DemoService {
     @Override
-    public String sayHello(String name) {
+    public String sayHello(String name)
+    {
+        /*try {
+            TimeUnit.SECONDS.sleep(15);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        System.out.println("say hello consume " + 15);
         return "hello from provider: " + name;
     }
 }
