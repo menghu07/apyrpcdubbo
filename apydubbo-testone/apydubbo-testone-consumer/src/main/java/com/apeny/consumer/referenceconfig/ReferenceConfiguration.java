9445af3393519a2a48913ad84dae53d451b893a2
apeny <812877318@qq.com>
2017-12-16 09:00:58 +0800

    api dubbo 配置

--- apydubbo-testone/apydubbo-testone-consumer/src/main/java/com/apeny/consumer/referenceconfig/ReferenceConfiguration.java
+++ apydubbo-testone/apydubbo-testone-consumer/src/main/java/com/apeny/consumer/referenceconfig/ReferenceConfiguration.java
@@ -1,9 +1,10 @@
 package com.apeny.consumer.referenceconfig;
 
-import com.alibaba.dubbo.config.ApplicationConfig;
-import com.alibaba.dubbo.config.ReferenceConfig;
-import com.alibaba.dubbo.config.RegistryConfig;
+import com.alibaba.dubbo.config.*;
 import com.apeny.api.service.DemoService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by apeny on 2017/12/15.
@@ -13,6 +14,7 @@ public class ReferenceConfiguration {
     private static ApplicationConfig applicationConfig = new ApplicationConfig();
     private static RegistryConfig registryConfig = new RegistryConfig();
     private static ReferenceConfig<DemoService> referenceConfig = new ReferenceConfig<>();
+    private static List<MethodConfig> methodConfigList = new ArrayList<>();
 
     static {
         applicationConfig.setName("api-consumer");
@@ -20,8 +22,20 @@ public class ReferenceConfiguration {
         referenceConfig.setApplication(applicationConfig);
         referenceConfig.setRegistry(registryConfig);
         referenceConfig.setInterface(DemoService.class);
-        referenceConfig.setTimeout(10_000);
+//        referenceConfig.setTimeout(10_000);
         referenceConfig.setVersion("1.0");
+        //方法级设置
+        MethodConfig methodConfig = new MethodConfig();
+        methodConfig.setName("sayHello");
+        ArgumentConfig argumentConfig = new ArgumentConfig();
+        argumentConfig.setType(String.class.getName());
+        List<ArgumentConfig> argumentConfigs = new ArrayList<>();
+        argumentConfigs.add(argumentConfig);
+//        methodConfig.setArguments(argumentConfigs);
+        methodConfig.setTimeout(10000);
+//        methodConfig.setRetries(3);
+        methodConfigList.add(methodConfig);
+        referenceConfig.setMethods(methodConfigList);
     }
 
     /**
@@ -30,8 +44,11 @@ public class ReferenceConfiguration {
      * @param args
      */
     public static void main(String[] args) {
+        long begin = System.nanoTime();
         DemoService demoService = referenceConfig.get();
         System.out.println("demoService: " + demoService.sayHello("new hello."));
+        long end = System.nanoTime();
+        System.out.println("use time: " + (end - begin) / 1000_000_000);
     }
 
 }
